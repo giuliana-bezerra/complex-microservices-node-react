f5d0aa431b77b197b0d5393bfbbc38acd1195ad1
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-04-14 16:25:08 -0300

    remove unused files

--- ticketing-auth/src/routes/api/users/signup.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/api/users/signup', (req, res) => {
-  res.send('Sign Up!');
-});
-
-export { router as signupRouter };
