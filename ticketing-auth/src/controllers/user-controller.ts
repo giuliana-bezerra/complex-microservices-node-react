1088ee9b4ea4e286ead474b4a71009603d52552b
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-04-28 16:08:31 -0300

    add error handling

--- ticketing-auth/src/controllers/user-controller.ts
+++ ticketing-auth/src/controllers/user-controller.ts
@@ -1,22 +1,24 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
 
-export function currentUser(req: Request, res: Response) {
+export async function currentUser(req: Request, res: Response) {
   res.send('Hi there!');
 }
 
-export function signin(req: Request, res: Response) {
+export async function signin(req: Request, res: Response) {
   res.send('Signed In!');
 }
 
-export function signout(req: Request, res: Response) {
+export async function signout(req: Request, res: Response) {
   res.send('Signed Out!');
 }
 
-export function signup(req: Request, res: Response) {
+export async function signup(req: Request, res: Response) {
   const errors = validationResult(req);
 
-  if (!errors.isEmpty()) return res.status(400).send(errors.array());
+  if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
 
   return res.send('Signed Up!');
 }
--- ticketing-auth/src/routes/index.ts
+++ ticketing-auth/src/routes/index.ts
@@ -1,6 +1,14 @@
+import 'express-async-errors';
+import { NotFoundError } from '../errors/not-found-error';
+import { errorHandler } from '../middlewares/error-handler';
 import { apiRouter } from './api';
 const router = require('express').Router();
 
 router.use('/api', apiRouter);
+router.all('*', async () => {
+  throw new NotFoundError();
+});
+
+router.use(errorHandler);
 
 export { router as routes };
